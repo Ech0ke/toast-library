0144fd7ce3e7c0086f29334f18873fdcbbc0ad46
Ech0ke <aidas.zygas@gmail.com>
2023-09-19 12:49:54 +0300

    Added form to add toasts for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useToast } from "./context/useToast";
 
 function App() {
-  return <>Hi</>;
+  const { addToast, removeToast } = useToast();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [id, setId] = useState<string>();
+
+  function createToast() {
+    if (inputRef.current == null || inputRef.current.value === "") return;
+
+    setId(
+      addToast(inputRef.current.value, {
+        autoDismiss: false,
+        position: "bottom-left",
+      })
+    );
+  }
+
+  return (
+    <div className="form">
+      <input type="text" ref={inputRef} />
+      <button onClick={createToast}>Add Toast</button>
+      <button
+        onClick={() => {
+          if (id !== null && id !== undefined) {
+            removeToast(id);
+          }
+        }}
+      >
+        Remove Last Toast
+      </button>
+    </div>
+  );
 }
 
 export default App;
